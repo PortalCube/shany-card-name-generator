84f73499f83593459d5797763119a6ad52c0bb70
PortalCube <35104213+PortalCube@users.noreply.github.com>
2021-01-15 10:45:32 +0900

    Release 1.0

---
+++ README.md
@@ -0,0 +1,63 @@
+# 샤이니 컬러즈 카드 타이틀 생성기
+
+아이돌마스터 샤이니 컬러즈 아이돌 카드 이름을 제작하는 도구입니다.
+
+Node.js에서 puppeteer의 스크린샷 캡쳐 기능을 활용하여, CSS와 SVG를 삽입한 HTML 페이지를 캡쳐하고 저장하는 방식으로 작동합니다.
+
+# 사용 방법
+
+### 설치하기
+
+1. Node.js가 없다면 설치합니다. https://nodejs.org/en/
+1. 저장소를 다운로드합니다.
+1. 저장소 디렉토리 (index.js가 있는 곳) 에서 터미널을 열고 "npm i" 커맨드를 이용해서 모든 패키지를 설치합니다.
+
+## Normal Build
+
+1. data-normal.csv 파일을 필요한대로 수정합니다.
+1. 수정이 완료되면 "npm run build"를 입력하여 이미지를 빌드합니다.
+1. 빌드가 완료되면 result 폴더에 생성된 이미지들이 저장됩니다. image.csv에는 번역 서버에 추가해야될 텍스트가 저장됩니다.
+
+## Simple Build
+
+심플 모드는 단순히 "이미지 생성" 이라는 목표에만 집중하는 모드로, 번역 서버에 필요한 데이터 등 이미지 생성에 불필요한 데이터를 생략하는 모드입니다.
+개인적인 용도로 카드 이미지를 생성하거나, 일부 이미지를 급히 업데이트할 때 유용합니다.
+
+1. data-simple.csv 파일을 필요한대로 수정합니다.
+1. 수정이 완료되면 "npm run simple"을 입력하여 이미지를 빌드합니다.
+1. 빌드가 완료되면 result 폴더에 생성된 이미지들이 저장됩니다.
+
+## CSV 형식
+
+각 파일의 예시는 저장소에 첨부된 파일을 참고해주세요.
+
+### data-normal.csv
+
+id,hash,version,type,rarity,idol_name_original,idol_name,card_name_original,card_name
+
+-   id: 카드 id (이미지 생성 및 image.csv 출력에 필요)
+-   hash: 카드 hash (image.csv 출력에 필요)
+-   version: 카드 타이틀 이미지의 version (image.csv 출력에 필요)
+-   type: 카드의 타입 (image.csv 출력에 필요)
+-   rarity: 카드의 레어리티 (이미지 생성에 사용)
+    -   1: N, 2: R, 3: SR, 4: SSR
+-   idol_name_original: 아이돌 이름 원문 (미사용)
+-   idol_name: 아이돌 이름 번역문 (이미지 생성에 사용)
+-   card_name_original: 카드 이름 원문 (미사용)
+-   card_name: 카드 이름 번역문 (이미지 생성에 사용)
+
+참고: (미사용) 이라고 표기된 열은 비워놔도 상관없습니다.
+
+### data-simple.csv
+
+id,rarity,idol_name,card_name
+
+-   id: 파일 이름 (이미지를 저장할 때, 이걸로 저장합니다)
+-   rarity: 카드의 레어리티 (이미지 생성에 사용)
+    -   1: N, 2: R, 3: SR, 4: SSR
+-   idol_name: 아이돌 이름 (이미지 생성에 사용)
+-   card_name: 카드 이름 (이미지 생성에 사용)
+
+# 저작권
+
+이 소프트웨어는 MIT 라이선스하에 배포됩니다. 사용자는 해당 라이선스의 조건하에 자유롭게 사용하실 수 있습니다.
---
+++ index.js
@@ -0,0 +1,158 @@
+const path = require("path");
+const util = require("util");
+const fs = require("fs");
+const readFile = util.promisify(fs.readFile);
+const writeFile = util.promisify(fs.writeFile);
+const Papa = require("papaparse");
+const sharp = require("sharp");
+const puppeteer = require("puppeteer");
+const logUpdate = require("log-update");
+const chalk = require("chalk");
+
+let time = 0;
+
+let latestFiles = [];
+let totalCount = 0;
+let doneCount = 0;
+
+async function Start(mode, namePrefix) {
+    const rawData = await readFile(`data-${mode}.csv`, { encoding: "utf8" });
+    const data = Papa.parse(rawData, { header: true }).data;
+    const timer = setInterval(
+        ProcessLog(mode.charAt(0).toUpperCase() + mode.slice(1)),
+        16
+    );
+
+    totalCount = data.length;
+    latestFiles.length = 10;
+    latestFiles.fill("");
+
+    await GenerateImageList(data, namePrefix);
+
+    clearInterval(timer);
+    logUpdate.done();
+
+    if (mode === "normal") {
+        console.log(chalk.blueBright("Create image.csv..."));
+
+        const resultData = [];
+        const directory = {
+            produce: "images/content/idols/name/",
+            support: "images/content/support_idols/name/"
+        };
+
+        for (let item of data) {
+            resultData.push([
+                `${directory[item.type]}${item.hash}_${item.id}.png`,
+                `${namePrefix}${item.id}.png`,
+                item.version
+            ]);
+        }
+
+        await writeFile("image.csv", Papa.unparse(resultData));
+    }
+
+    console.log(chalk.green("Complete! Check the 'result' directory."));
+}
+
+async function GenerateImageList(data, name) {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    await page.goto(`file:${path.join(__dirname, "image.html")}`);
+    await page.setViewport({ width: 480, height: 360, deviceScaleFactor: 2 });
+
+    async function GetImage(item) {
+        await page.evaluate((item) => {
+            const rarity = ["r", "r", "sr", "ssr"];
+            SetText(item.card_name, item.idol_name, rarity[item.rarity - 1]);
+        }, item);
+
+        const buffer = await page.screenshot({
+            omitBackground: true,
+            clip: { x: 0, y: 0, width: 444, height: 198 }
+        });
+
+        // Create image as 2x size, and resize it into 1x size.
+        // I chose this way, because Puppeteer is create quite dirty image when I create 1x size image.
+        // This way spend more time (about 10 ~ 20%) but create much better images :)
+        await sharp(buffer).resize(444, 198).toFile(`result/${name}${item.id}.png`);
+    }
+
+    for (let item of data) {
+        const start = new Date();
+        await GetImage(item);
+        time += new Date() - start;
+        latestFiles.push(`${++doneCount}. [${item.card_name}] ${item.idol_name}`);
+    }
+
+    await browser.close();
+}
+
+function GetProgressBar(percentage, length) {
+    const count = Math.floor(percentage * length);
+    const result = [];
+    for (let i = 0; i < length; i++) {
+        if (i < count) {
+            result.push("▶");
+        } else {
+            result.push("▷");
+        }
+    }
+    return result.join("");
+}
+
+function GetEstimatedTime() {
+    if (time === 0 || doneCount === 0) {
+        return "";
+    } else {
+        const average = time / doneCount;
+        const left = totalCount - doneCount;
+        const estimate = Math.round((average * left) / 1000);
+        return `${Math.floor(estimate / 60)}m ${estimate % 60}s`;
+    }
+}
+
+function ProcessLog(mode) {
+    return () => {
+        const percentage = doneCount / totalCount;
+        const percentageString = (percentage * 100).toLocaleString("en-US", {
+            minimumFractionDigits: 2,
+            maximumFractionDigits: 2
+        });
+
+        logUpdate(
+            [
+                chalk.blueBright(`Mode: ${mode}`),
+                ...latestFiles.slice(-10),
+                "",
+                chalk.yellow(`Processing... (${doneCount}/${totalCount})`),
+                chalk.yellowBright(`Estimate: ${GetEstimatedTime()}`),
+                chalk.yellow(`${GetProgressBar(percentage, 20)} ${percentageString}%`),
+                ""
+            ].join("\n")
+        );
+    };
+}
+
+(async function () {
+    const mode = process.env.MODE;
+
+    if (mode === undefined) {
+        console.error(
+            chalk.bgRed("ERROR: You need to run this tool with 'npm run <command>'.")
+        );
+    }
+
+    switch (mode) {
+        case "normal":
+            await Start("normal", "card_name_");
+            break;
+        case "simple":
+            await Start("simple", "");
+            break;
+        default:
+            console.error(chalk.bgRed(`ERROR: Unknown command "${mode}"`));
+            break;
+    }
+})();
